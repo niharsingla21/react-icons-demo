1a17d689325001c586418e32be9777baa51e7b38
Nihar Singla <nihar.singla@mettl.com>
2021-01-21 16:03:58 +0530

    react icons project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { FaReact } from "react-icons/fa";
+import { MdAlarmOn } from "react-icons/md";
+import { IconContext } from "react-icons";
 import './App.css';
 
 function App() {
   return (
+    <IconContext.Provider value = {{ color:'cyan' ,size:'5rem'}}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FaReact/>
+      <MdAlarmOn/>
     </div>
+    </IconContext.Provider>
   );
 }
 
